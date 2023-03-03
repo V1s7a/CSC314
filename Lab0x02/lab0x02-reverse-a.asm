a30da247afb70a592c2557306a3fc8a2eef03f2a
V1s7a <cdwillits@live.com>
2023-03-02 18:51:57 -0600

    reverse-a complete

--- Lab0x02/lab0x02-reverse-a.asm
+++ Lab0x02/lab0x02-reverse-a.asm
@@ -13,7 +13,7 @@ section .text
 _start:
 
 
-mimic_loop:
+reverse_loop:
  
     mov eax, SYS_READ ;;set ups sys read
     mov ebx, FD_STDIN 
@@ -26,20 +26,34 @@ mimic_loop:
 
     
     cmp dword [inputByteCount], 0  ;;check EOF
-    jle end_mimic_loop ;;break
+    jle end_reverse_loop ;;break
 
+    ;;the first character to print is the last character read, except the newline character
+    ;;so its position will be inputByteCount-2 (the newline will be at inputByteCount-1)
+
+    mov esi, [inputByteCount] ;;using esi for character postion
+    sub esi, 2 ;; starting character position at inputByteCount-1 (last character before the newline)
+
+
+single_char_loop:
+    ;; strategy A: a loop to print one character at a time
     mov eax, SYS_WRITE ;; setup sys_write
     mov ebx, FD_STDOUT
-    mov ecx, inputBuffer 
-    mov edx, [inputByteCount] 
+    mov ecx, inputBuffer ;; want inputBuffer plus character position
+    add ecx, esi         ;; add the characterPostion to the start address of inputBuffer
+    mov edx, 1           ;;we only want to print one character at a time
     int 0x80 ;; trigger sys_write
 
-    jmp mimic_loop ;;loop
+    dec esi ;;decrement character position
+    cmp esi, 0 ;;compare if last character is zero
+    jl end_single_char_loop ;; jump if less than zero, can use js (jump if sign, negative)
+    jmp single_char_loop
 
-end_mimic_loop:
+end_single_char_loop:
 
+    jmp reverse_loop ;;loop
 
-
+end_reverse_loop:
 ;EXIT
     mov eax, 1
     mov ebx, 0
